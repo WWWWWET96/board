ec85cbc0c586980846ac7bef610d4f713c998060
이정은 <wwwwwet96@naver.com>
2022-01-28 03:14:30 +0900

    최종2

--- src/main/java/sconelee/board/BoardApplication.java
+++ src/main/java/sconelee/board/BoardApplication.java
@@ -6,10 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
-
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 
 		SpringApplication.run(BoardApplication.class, args);
